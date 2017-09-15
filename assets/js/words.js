350404c584a0a5e8d7903a07922e2f9506808398
Chewsterchew <chew@chew.pw>
2017-09-14 20:33:28 -0500

    start preparing words

--- assets/js/words.js
+++ assets/js/words.js
@@ -1,13 +1,53 @@
-var word1 = "bites,highs,burns,ruins,humids,leans,quiets,traffics,homes,crashes,trumps,backs,salts,xboxs,closes,records,stops,sevens,pollutes,kills,rents,cleans,extras,boggles,Taylor's,snaps,questions,coffee's,clicks,pops,ticks,maintains,stars,ties,nys,bills,defends,opens,airs,Americans,steals,drinks,yous,businesses,teleys,invents,thanks,students,computers,frees,weathers,vends,severs,allergies,silences,fires,ambers,pushes,screws,smokes,mrs,reds,consumes,let's,classes,makes,draws,lights,butters,celebrates,drives,pulls,toxics,finds,waters,pets,lags,types,environments,grows,builds,moos,tunas,confuses,classifies,births,fails,breaks,emotionals,booms,calls,taxes,burgers,4s,gases,potatoes,pre owns,sends,mows,tickles,lefts,Saharas,nals,unites,camps,roses,shuts down,macs,apples,cheeses,turns,flexes,moves,trucks,necks,swallows,Harry's,flushes,pays,eyes,cities,increases,trains,cooks,i's,cringes,unders,folds,enters,speeds,roads,spends,tacos,pumps,hearts,Willows,reads,suhs,dogs,rocks,cookies"
+var awords = ""
+var bwords = ""
+var cwords = ""
+var dwords = ""
+var ewords = ""
+var fwords = ""
+var gwords = ""
+var hwords = ""
+var iwords = ""
+var jwords = ""
+var kwords = ""
+var lwords = ""
+var mwords = ""
+var nwords = ""
+var owords = ""
+var pwords = ""
+var qwords = ""
+var rwords = ""
+var swords = ""
+var twords = ""
+var uwords = ""
+var vwords = ""
+var wwords = ""
+var xwords = ""
+var ywords = ""
+var zwords = ""
 
-var word2 = "bites,voices,rubber,jokes,weather,dabs,time,jams,depots,parties,country,Clinton,fires,grasses,one,door,videos,signs,elevens,air,mood,movie,rooms,roads,brain cells,points,mind,Swifts,chats,vibe,motives,mugs,pens,buttons,sanity,tocks,office,scouts,shoes,keys,nyes,freedom,will to live,force,flags,Gatorade,sprite,tubes,service,phones,wheel,yous,services,labs,tuition,ford,machines,warnings,alert,phone,extinguishers,dexterious,driver,detector,jos,cross,M&Ms,goes,days,pictures,poles,biscuit,75 years,cars,levers,waters,ways out,burgers,dogs,minecraft,emojis,sciences,trees,legos,buildings,cows,fish,conversation,animals,certificates,science classes,hearts,issues,roasted,horns,friends,kings,Gs,birthdays,stations,chips,vehicles,texts,lawns,pickles,lanes,deserts,genes,rocks,states,outs,coffee,reds,computers,books,watches,milk,steaks,teens,wheels,muscles,homes,stops,self,tattoos,food,Potters,toilets,brows,limits,toasts,towers,volume,tracks,wears,bones,oragamies,zones,kills,money,bells,ups,radios,ways,Donald's,springs,elections,walls,corn,dudes,filters,rolls,tongues"
-
-var word1array = word1.split(",");
-
-var word2array = word2.split(",");
-
-var lines2 = [
-  ["That really"],
-  word1array, ["my"],
-  word2array,
-];
+var aarray = awords.split(",");
+var barray = bwords.split(",");
+var carray = cwords.split(",");
+var darray = dwords.split(",");
+var earray = ewords.split(",");
+var farray = fwords.split(",");
+var garray = gwords.split(",");
+var harray = hwords.split(",");
+var iarray = iwords.split(",");
+var jarray = jwords.split(",");
+var karray = kwords.split(",");
+var larray = lwords.split(",");
+var marray = mwords.split(",");
+var narray = nwords.split(",");
+var oarray = owords.split(",");
+var parray = pwords.split(",");
+var qarray = qwords.split(",");
+var rarray = rwords.split(",");
+var sarray = swords.split(",");
+var tarray = twords.split(",");
+var uarray = uwords.split(",");
+var varray = vwords.split(",");
+var warray = wwords.split(",");
+var xarray = xwords.split(",");
+var yarray = ywords.split(",");
+var zarray = zwords.split(",");
