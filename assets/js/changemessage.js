531b10884d7f586d905511d8ce3dab220172188e
Chewsterchew <chew@chew.pw>
2018-01-09 19:20:02 -0600

    hey change this up a bit

--- assets/js/changemessage.js
+++ assets/js/changemessage.js
@@ -1,6 +1,4 @@
 function changeMessage() {
-  for (var i = 0; i < 4; i++) {
-    document.getElementById(i + "line").innerHTML = lines2[i][(Math.floor(Math.random() * lines2[i].length))];
-  }
+  document.getElementById("phrase").innerHTML = phrase;
   document.getElementById("startBttn").textContent = "Again?";
 }
