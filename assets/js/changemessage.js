354a7ce2e4adf9dd23798bea709ce2061627105c
Chew <chew@chew.pw>
2021-09-12 18:54:24 -0500

    Move words to JSON here too

--- assets/js/changemessage.js
+++ assets/js/changemessage.js
@@ -1,8 +1,20 @@
-function changeMessage(phrase) {
-  document.getElementById("generatedphrase").innerHTML = document.getElementById("acronym").value + " stands for: " + phrase;
+let words;
+
+fetch('./assets/json/words.json').then(r => r.json()).then(data => words = data);
+
+/**
+ * Changes the message displayed on the main page
+ * @param acronym {string} The original acronym
+ * @param phrase {string} The filled out phrase
+ */
+function changeMessage(acronym, phrase) {
+  document.getElementById("generatedphrase").innerHTML = acronym + " stands for: " + phrase;
   document.getElementById("startBttn").textContent = "Again?";
 }
 
+/**
+ * Generates an acronym and changes the message
+ */
 function generateAcronym() {
   let acronym = document.getElementById("acronym").value;
   acronym = acronym.toLowerCase();
@@ -12,14 +24,20 @@ function generateAcronym() {
     phrase += generateWord(letters[i]);
     phrase += " ";
   }
-  changeMessage(phrase);
+  changeMessage(acronym, phrase);
 }
 
+/**
+ * Generates a word for a given letter, or "" for invalid character
+ * @param letter {string} The letter
+ * @returns {string} A random word starting with the specified letter
+ */
 function generateWord(letter) {
-  let array = "";
+  let array = [];
   if (letter.toString().toLowerCase().match(/[a-z]/))
     array = words[letter].split(',');
-  if (array !== "")
+
+  if (array.length > 0)
     return array[Math.floor(Math.random() * array.length)];
   else
     return "";
